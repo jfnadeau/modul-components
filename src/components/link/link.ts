f14507f36c4bba5644ca5de8f2be8f1566f89b9e
Jean-Francois Nadeau <jean-francois.nadeau@dti.ulaval.ca>
2018-02-27 08:18:09 -0500

    tslint

--- src/components/link/link.ts
+++ src/components/link/link.ts
@@ -75,7 +75,7 @@ export class MLink extends ModulVue {
     @Prop({ default: '12px' })
     public iconSize: string;
 
-    private onClick(event) {
+    private onClick(event): void {
         this.$el.blur();
         if (this.isButton || this.isTextLink || this.disabled) {
             event.preventDefault();
